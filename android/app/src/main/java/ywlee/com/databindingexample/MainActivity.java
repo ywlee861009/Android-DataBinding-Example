88c0f5041aaff136009d683e76a536603ab37b22
ywlee <ywlee@j-compia.com>
2018-06-08 10:07:59 +0900

    데이터 바인딩 TextView 달아보기

--- android/app/src/main/java/ywlee/com/databindingexample/MainActivity.java
+++ android/app/src/main/java/ywlee/com/databindingexample/MainActivity.java
@@ -1,16 +1,24 @@
 package ywlee.com.databindingexample;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import ywlee.com.databindingexample.databinding.ActivityMainBinding;
+
 public class MainActivity extends Activity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // DataBinding 은 setContentView 가 필요없다.
+//        setContentView(R.layout.activity_main);
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        // 개쩜..바로 사용할수 있음..
+        binding.mainTxvBindingTest.setText("Test");
     }
 }
